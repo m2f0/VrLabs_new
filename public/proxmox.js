67fe51cccba047732e39f8cd8e8de7421ed67a08
Mario Mayerle Filho <mariomayerlefilho@live.com>
2024-12-12 09:36:17 -0300

    updated proxmox.js

--- public/proxmox.js
+++ public/proxmox.js
@@ -1,21 +1,24 @@
 function startVM(vmid, node, name) {
-    fetch(`${window.API_BASE_URL}/api2/json/nodes/${node}/qemu/${vmid}/status/start`, {
-      method: "POST",
-      headers: {
-        Authorization: window.API_TOKEN,
-      },
+  fetch(`${window.API_BASE_URL}/api2/json/nodes/${node}/qemu/${vmid}/status/start`, {
+    method: "POST",
+    headers: {
+      Authorization: window.API_TOKEN,
+    },
+  })
+    .then((response) => {
+      console.log("Resposta do servidor:", response);
+      if (!response.ok) {
+        throw new Error("Erro ao iniciar a VM");
+      }
+      alert(`VM ${name} iniciada com sucesso!`);
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Erro ao iniciar a VM");
-        }
-        alert(`VM ${name} iniciada com sucesso!`);
-      })
-      .catch((error) => alert(error.message));
-  }
-  
-  function connectVM(vmid, node) {
-    const url = `${window.API_BASE_URL}/?console=kvm&novnc=1&vmid=${vmid}&node=${node}`;
-    window.open(url, "_blank");
-  }
-  
+    .catch((error) => {
+      console.error("Erro ao iniciar a VM:", error);
+      alert(error.message);
+    });  
+}
+
+function connectVM(vmid, node) {
+  const url = `${window.API_BASE_URL}/?console=kvm&novnc=1&vmid=${vmid}&node=${node}`;
+  window.open(url, "_blank");
+}
