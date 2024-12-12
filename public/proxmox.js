5eb8b65396bfd90e7ac5ed666235ec23416908d3
Mario Mayerle Filho <mariomayerlefilho@live.com>
2024-12-12 11:50:08 -0300

    updated

--- public/proxmox.js
+++ public/proxmox.js
@@ -1,22 +1,30 @@
-function startVM(vmid, node, name) {
-  fetch(`${window.API_BASE_URL}/api2/json/nodes/${node}/qemu/${vmid}/status/start`, {
-    method: "POST",
-    headers: {
-      Authorization: window.API_TOKEN,
-    },
-  })
-    .then((response) => {
-      console.log("Resposta do servidor:", response);
-      if (!response.ok) {
-        throw new Error(`Erro ao iniciar a VM: ${response.statusText}`);
-      }
-      alert(`VM ${name} iniciada com sucesso!`);
-    })
-    .catch((error) => {
-      console.error("Erro ao iniciar a VM:", error);
-      alert(error.message);
+async function startVM(vmid, node, name) {
+  const ticket = getCookie("PVEAuthCookie"); // Função para obter o cookie
+  if (!ticket) {
+    alert("Erro: Ticket de autenticação não encontrado.");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${window.API_BASE_URL}/api2/json/nodes/${node}/qemu/${vmid}/status/start`, {
+      method: "POST",
+      headers: {
+        Authorization: `PVEAuthCookie=${ticket}`,
+      },
     });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Erro ao iniciar a VM: ${errorText}`);
+    }
+
+    alert(`VM ${name} iniciada com sucesso!`);
+  } catch (error) {
+    console.error("Erro ao iniciar a VM:", error);
+    alert(error.message);
+  }
 }
+
 
 
 function connectVM(vmid, node) {
