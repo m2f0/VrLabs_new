c208000dbe6465ac0b1e33931ab2a1a132c1e200
Mario Mayerle Filho <mariomayerlefilho@live.com>
2024-12-31 11:57:41 -0300

    updated moodle-utils.js

--- public/moodle-utils.js
+++ public/moodle-utils.js
@@ -1,18 +1,20 @@
 async function checkMoodleSession() {
-    try {
-      const response = await fetch('/local/easyit_cyberarena/check_user.php');
+  try {
+      // Ajuste o endpoint para usar o rest.php
+      const response = await fetch('/local/easyit_cyberarena/rest.php?action=checkUser');
       if (!response.ok) {
-        throw new Error('Erro ao verificar a sessão do Moodle.');
+          throw new Error('Erro ao verificar a sessão do Moodle.');
       }
       const data = await response.json();
+
+      // Verifica o status retornado pelo servidor
       if (data.status !== 'success') {
-        alert('Usuário não autenticado no Moodle.');
-        window.location.href = '/login/index.php';
+          alert('Usuário não autenticado no Moodle.');
+          window.location.href = '/login/index.php';
       } else {
-        console.log('Usuário autenticado:', data.username);
+          console.log('Usuário autenticado:', data.username);
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Erro ao verificar sessão do Moodle:', error);
-    }
   }
-  
+}
